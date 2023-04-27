e0ac9c3024a99fa156133c5d21db0281a8e7f32d
PetarPaunov <petarp.2000@abv.bg>
2023-04-27 23:36:22 +0300

    AddRange extension method added to ICollection interface

---
+++ src/ExpenseTracking.Web/Extensions/ICollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseTracking.Web.Extensions
+{
+    public static class ICollectionExtension
+    {
+        public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T> source)
+        {
+            foreach (var item in source)
+            {
+                destination.Add(item);
+            }
+        }
+    }
+}
