049cee2d431b19da15879963f407700885c3b32b
PetarPaunov <petarp.2000@abv.bg>
2023-05-03 22:03:13 +0300

    Added category service

---
+++ src/ExpenseTracking.Core/Contracts/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracking.Core.Contracts
+{
+    using ExpenseTracking.Core.Models.CategoryViewModels;
+
+    public interface ICategoryService
+    {
+        public Task<IEnumerable<GetCategoriesViewModel>> GetCategoriesAsync();
+    }
+}
---
+++ src/ExpenseTracking.Core/Services/CategoryService.cs
@@ -0,0 +1,35 @@
+﻿namespace ExpenseTracking.Core.Services
+{
+    using ExpenseTracking.Core.Contracts;
+    using ExpenseTracking.Core.Models.CategoryViewModels;
+    using ExpenseTracking.Infrastructure.GenericRepository;
+    using ExpenseTracking.Infrastructure.Models.ExpenseTables;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IGenericRepository repository;
+
+        public CategoryService(IGenericRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        // Add comment
+        public async Task<IEnumerable<GetCategoriesViewModel>> GetCategoriesAsync()
+        {
+            var categories = await this.repository
+                .AllReadonly<Category>()
+                .Select(x => new GetCategoriesViewModel()
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Icon = x.Icon
+                })
+                .ToListAsync();
+
+            return categories;
+        }
+    }
+}
