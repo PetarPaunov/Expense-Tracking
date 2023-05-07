dfd0ea6920a010ede7b7065110c34240f7adb620
PetarPaunov <petarp.2000@abv.bg>
2023-05-07 17:58:47 +0300

    All transaction add into table with pagination and serach

--- src/ExpenseTracking.Web/wwwroot/js/site.js
+++ src/ExpenseTracking.Web/wwwroot/js/site.js
@@ -2,3 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#table-pager').DataTable({
+        lengthMenu: [
+            [7],
+            [7],
+        ],
+        info: false,
+        ordering: false,
+        columnDefs: [{
+            targets: 2,
+            render: $.fn.dataTable.render.ellipsis( 5)
+        }]
+    });
+});
+
+$.fn.dataTable.render.ellipsis = function (cutoff, wordbreak, escapeHtml) {
+    var esc = function (t) {
+        return t
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;');
+    };
+
+    return function (d, type, row) {
+        // Order, search and type get the original data
+        if (type !== 'display') {
+            return d;
+        }
+
+        if (typeof d !== 'number' && typeof d !== 'string') {
+            return d;
+        }
+
+        d = d.toString(); // cast numbers
+
+        if (d.length < cutoff) {
+            return d;
+        }
+
+        var shortened = d.substr(0, cutoff - 1);
+
+        // Find the last white space character in the string
+        if (wordbreak) {
+            shortened = shortened.replace(/\s([^\s]*)$/, '');
+        }
+
+        // Protect against uncontrolled HTML input
+        if (escapeHtml) {
+            shortened = esc(shortened);
+        }
+
+        return '<span class="ellipsis" title="' + esc(d) + '">' + shortened + '&#8230;</span>';
+    };
+};
